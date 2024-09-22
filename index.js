17ad236a778cd1b5ddd3a71e7c02312ceb4f1f9c
Black-Panther30121995 <160006925+Black-Panther30121995@users.noreply.github.com>
2024-09-22 03:39:59 +0000

    create todoList

---
+++ index.js
@@ -0,0 +1,80 @@
+const todoList = JSON.parse(localStorage.getItem('todoList'))||
+        {
+            name:'',
+            dueDate:'',
+        };
+
+        renderTodoList();
+
+        document.querySelector('.addButton').addEventListener('click', () => {
+            addToLocalStorage();
+            todoadd();
+        })
+
+        function addToLocalStorage()
+        {
+            localStorage.setItem('todoList',JSON.stringify(todoList));
+        }
+
+        function todoadd() {
+            let inputEle = document.querySelector('.todoName');
+            let name = inputEle.value;
+            let inputDate = document.querySelector('.todoDate');
+            let dueDate = inputDate.value;
+            todoList.push({
+                name,
+                dueDate,
+            });
+
+            inputEle.value = '';
+            inputDate.value = '';
+
+            addToLocalStorage();
+            renderTodoList();
+        }
+
+        function renderTodoList() {
+            let todoListHTML = '';
+            todoList.forEach((todoObject, i) => {
+               /* if (i == 0) {
+                    return;
+                }*/
+                const { name, dueDate } = todoObject;
+                todoListHTML +=
+                    `<div>${name}</div>
+           <div>${dueDate}</div>
+           <button class="delButton" >Delete
+           </button>
+            `;
+                addToLocalStorage();
+            });
+
+            /*
+                for (let i = 1; i < todoList.length; i++) {
+                    const todoObject = todoList[i];
+                    const { name, dueDate } = todoObject;
+                    const html =
+                        `<div>${name}</div>
+               <div>${dueDate}</div>
+               <button class="delButton" onclick="
+               todoList.splice(${i},1);
+               renderTodoList();
+               ">Delete
+               </button>
+                `;
+                    todoListHTML += html;
+                };
+                */
+
+            document.querySelector('.todoList').innerHTML = todoListHTML;
+
+
+            document.querySelectorAll('.delButton').forEach((button, i) => {
+                button.addEventListener('click', () => {
+                    todoList.splice(i, 1);
+                    addToLocalStorage();
+                    renderTodoList();
+                });
+            });
+
+        }
